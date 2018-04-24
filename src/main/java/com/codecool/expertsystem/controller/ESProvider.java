bb5005d99b13410a850dde2e994c764d5935d4b0
johnny-sack1 <jonymaka@gmail.com>
2018-04-24 13:44:15 +0200

    Create ESProvider class skeleton

---
+++ src/main/java/com/codecool/expertsystem/controller/ESProvider.java
@@ -0,0 +1,21 @@
+public class ESProvider {
+    private FactParser factParser;
+    private RuleParser ruleParser;
+
+    public ESProvider(FactParser factParser, RuleParser ruleParser) {
+        this.factParser = factParser;
+        this.ruleParser = ruleParser;
+    }
+
+    public void collectAnswers() {
+
+    }
+
+    public boolean getAnswerByQuestion(String question) {
+
+    }
+
+    public String evaluate() {
+        
+    }
+}
