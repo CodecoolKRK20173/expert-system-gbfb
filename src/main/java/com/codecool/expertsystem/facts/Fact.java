83c1888c585a9b6988c776de60b7b7a4aa39bc8b
johnny-sack1 <jonymaka@gmail.com>
2018-04-24 13:22:24 +0200

    Implement Fact class

--- src/main/java/com/codecool/expertsystem/facts/Fact.java
+++ src/main/java/com/codecool/expertsystem/facts/Fact.java
@@ -1,12 +1,47 @@
 package com.codecool.expertsystem.facts;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
 public class Fact {
 
-    String id;
-    String description;
+    private String id;
+    private String description;
+    private Map<String, Boolean> IdValueMap;
 
     public Fact(String id, String description) {
         this.id = id;
         this.description = description;
+        this.IdValueMap = new HashMap<>();
+        initIdValueMap();
+    }
+
+    private void initIdValueMap() {
+        String[] ids = {"looks", "speed", "reliability", "janusz"};
+        for (String id : ids) {
+            this.IdValueMap.put(id, false);
+        }
+    }
+
+    public Set<String> getIdSet() {
+        return this.IdValueMap.keySet();
+    }
+
+    public void setFactValueById(String id, boolean value) {
+        for (String key : this.IdValueMap.keySet()) {
+            if (key.equals(id)) {
+                this.IdValueMap.put(key, value);
+            }
+        }
+    }
+
+    public boolean getValueById(String id) {
+        for (String key : this.IdValueMap.keySet()) {
+            if (key.equals(id)) {
+                return this.IdValueMap.get(id);
+            }
+        }
+        return false;
     }
 }
