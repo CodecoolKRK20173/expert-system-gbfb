0397c88d6998fb4477b7a779f539d1dc48ec45be
Bart <bartpetka@gmail.com>
2018-04-24 11:18:28 +0200

    Implement Question constructor

---
+++ src/main/java/com/codecool/expertsystem/rules/Question.java
@@ -0,0 +1,14 @@
+package com.codecool.expertsystem.Question;
+
+public class Question {
+
+    String ID;
+    String question;
+    Answer answer;
+
+    public Question(String ID, String question, Answer answer) {
+        this.ID = ID;
+        this.question = question;
+        this.Answer = answer;
+    }
+}
