9a72982d69f97cbbbd9c231f77783b4b2f4eeaf8
saturator22 <f.szczeski@gmail.com>
2018-04-24 14:26:17 +0200

    Add FactParser

---
+++ src/main/java/com/codecool/expertsystem/parsers/FactParser.java
@@ -0,0 +1,21 @@
+package com.codecool.expertsystem.parsers;
+
+import com.codecool.expertsystem.facts.*;
+
+import org.w3c.dom.Document;
+
+public class FactParser extends XMLParser {
+    FactRepository factsRepo = new FactRepository;
+
+    public FactParser() {
+        loadXmlDocument("facts.xml");
+    }
+
+    public FactRepository getFactRepository() {
+        return new FactRepository;
+    }
+
+    private void fillFactsRepo() {
+
+    }
+}
