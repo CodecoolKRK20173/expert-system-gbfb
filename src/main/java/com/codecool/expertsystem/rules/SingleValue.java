3c200290ba408f5d1559fb340b098387c25bb95d
Marcin Tuleja <marcintuleja@gmail.com>
2018-04-26 14:28:24 +0200

    Refactor setting input pattern

--- src/main/java/com/codecool/expertsystem/rules/SingleValue.java
+++ src/main/java/com/codecool/expertsystem/rules/SingleValue.java
@@ -1,8 +1,18 @@
 package com.codecool.expertsystem.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SingleValue extends Value {
     public SingleValue(String param, boolean selectionType) {
         super(selectionType);
-        getInputPattern().add(param);
+        setInputPattern(parseStringToList(param));
+    }
+
+    private List<String> parseStringToList (String param) {
+        List<String> params = new ArrayList<>();
+        params.add(param);
+
+        return params;
     }
 }
